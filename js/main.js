c46b41704c881c96e80ccc0691b0473018fe3758
Alex <atuhbatshina@yandex.ru>
2024-10-09 14:52:39 +0500

    добавляет фото(изменен после код-ревью)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const MESSAGE = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -6,7 +6,7 @@ const MESSAGE = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
-const NAME = [
+const NAMES = [
   'Артем',
   'Алексей',
   'Ольга',
@@ -36,34 +36,31 @@ const DESCRIPTIONS = [
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+  return Math.floor(Math.random() * (upper - lower + 1) + lower);
 };
 function generateComments(){
   const comentCount = getRandomInteger(0,30);
   const coments = [];
   for (let i = 0; i < comentCount; i++){
-    const coment = {
+    coments.push({
       id: i + 1,
-      avatar: 'img/avatar-{getRandomInteger(1,6)}.svg',
-      message: MESSAGE[getRandomInteger(0, MESSAGE.length - 1)],
-      name:NAME[getRandomInteger(0,NAME.length - 1)],
-    };
-    coments.push(coment);
+      avatar: `img/avatar-${getRandomInteger(1,6)}.svg`,
+      message: MESSAGES[getRandomInteger(0, MESSAGES.length - 1)],
+      name: NAMES[getRandomInteger(0, NAMES.length - 1)],
+    });
   }
   return coments;
 }
 function generatePhotos(){
   const photos = [];
   for(let i = 1; i <= 25;i++){
-    const photo = {
+    photos.push({
       id: i,
-      url:'photos/{i}.jpg',
-      description:DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
-      likes:getRandomInteger(15,200),
-      comments:generateComments(),
-    };
-    photos.push(photo);
+      url: `photos/${i}.jpg`,
+      description: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
+      likes: getRandomInteger(15,200),
+      comments: generateComments(),
+    });
   }
   return photos;
 }
